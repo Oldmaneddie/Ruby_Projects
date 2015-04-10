c7eb60cd8c62cd13fe9b6b4d58233266120249e0
Eddie Park <edward.saejoon.park@gmail.com>
2015-04-09 21:45:28 -0400

    Added basic operations
    Simple calculator program with Basic operations

---
+++ ruby_calc.rb
@@ -0,0 +1,31 @@
+def calc 									
+	print "Please enter first digit: "				
+	first_digit = gets.chomp						
+	print "Please enter second digit: "
+	second_digit = gets.chomp						
+	case @response									
+		when "+" # add						
+			puts "Result is #{first_digit.to_i + second_digit.to_i}."
+		when "-" # sub
+			puts "Result is #{first_digit.to_i - second_digit.to_i}."
+		when "*" # multi
+			puts "Result is #{first_digit.to_i * second_digit.to_i}."
+		when "/" # div
+			puts "Result is #{first_digit.to_i / second_digit.to_i}."
+
+		end
+	end
+
+################### Main Execution Bloc
+puts "--------"
+puts " WELCOME TO RUBY CALCULATOR!"
+puts "--------"
+while true										
+  print "Enter an operator (+) (-) (/) (*) or enter (q) to quit: "
+  @response = gets.chomp
+    if (@response == "q")  				
+      break
+    else									
+      calc
+    end
+end
