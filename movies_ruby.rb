f3fabaf812c0c16ccfd6787bc5699321368e9416
Eddie Park <edward.saejoon.park@gmail.com>
2015-04-14 15:06:35 -0400

    included add option

--- movies_ruby.rb
+++ movies_ruby.rb
@@ -16,3 +16,28 @@ puts "-- Type 'display' to display all movies."
 puts "-- Type 'delete' to delete a movie."
 
 choice = gets.chomp.downcase
+case choice
+when 'add'
+  puts "Oh Awesome! New Flicks!? What is it called?"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "What's the rating? (Type a number 0 to 4.)"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+    puts "#{title} NICE! has been added with a rating of #{rating}."
+  else
+    puts "WOAH That movie already exists! Its rating is #{movies[title.to_sym]}."
+  end
+
+when 'update'
+  end
+
+when 'display'
+  end
+
+when 'delete'
+  end
+
+else
+  puts "Sorry, I didn't understand you."
+end
