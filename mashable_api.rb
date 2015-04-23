ffb4f4869730f4eaab4a16fb0153bc563faca894
Eddie Park <parkes.edward@gmail.com>
2015-04-23 10:39:17 -0400

    Setup rest-client call and built basic method outline & main program bloc

---
+++ mashable_api.rb
@@ -0,0 +1,34 @@
+# http://mashable.com/stories.json
+
+# Include rest-client library
+# Include JSON library
+# Setup rest-client call
+# Parse the results from the Reddit API
+# Build an array of hashes
+# Every hash is going to represent a single story
+
+require 'rest-client'
+require 'json'
+
+#######################
+# METHODS
+#######################
+
+#get data from mashable
+def get_from_mashable
+end
+
+#display adata 
+
+def display_stories (search_json)
+
+end
+
+
+
+#######################
+# MAIN PROGRAM BLOC
+#######################
+
+search_json = get_from_mashable
+display_stories = (search_json)
